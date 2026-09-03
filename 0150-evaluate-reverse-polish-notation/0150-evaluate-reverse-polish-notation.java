bd1b55fb501eb9f439ebdd347c4f4c30f1112640
Vinayak Kumbhar <kumbharvinayak751@gmail.com>
2026-09-03 10:01:43 +0530

    Time: 84 ms (5.59%), Space: 47 MB (5.84%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<tokens.length; i++){
+            String ch = tokens[i];
+            boolean isDigit = ch.length() == 1 ? Character.isDigit(ch.charAt(0)) : Character.isDigit(ch.charAt(1));
+            System.out.println("String is : " + ch);
+
+            if(isDigit){
+                stack.push(Integer.parseInt(ch));
+            }else{
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                System.out.println("num is : " + num1 + "   " + num2);
+
+                if(ch.equals("+")){
+                    stack.push(num1 + num2);
+                }
+                if(ch.equals("-")){
+                    stack.push(num2 - num1);
+                }
+                if(ch.equals("*")){
+                    stack.push(num1 * num2);
+                }
+                if(ch.equals("/")){
+                    stack.push(num2 / num1);
+                }
+            }
+        }
+        return stack.pop();
+    }
+}
